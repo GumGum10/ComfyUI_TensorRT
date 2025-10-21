ed7de1630a86b72d6d75776bfd66fd0900b25fd3
GumGum10 <xdominal8@example.com>
2025-10-21 20:23:48 +0300

    Remove opset_version

--- tensorrt_convert.py
+++ tensorrt_convert.py
@@ -315,7 +315,7 @@ class TRT_MODEL_CONVERSION_BASE:
             verbose=False,
             input_names=input_names,
             output_names=output_names,
-            opset_version=17,
+            opset_version=None,
             dynamic_axes=dynamic_axes,
         )
 
